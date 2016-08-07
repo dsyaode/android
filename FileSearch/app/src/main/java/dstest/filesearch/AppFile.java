ffde526329007fe0802613f7d55ee07e7ee046e9
dsyaode <442977011@qq.com>
2016-08-07 16:34:24 +0800

    2016-08-07

---
+++ FileSearch/app/src/main/java/dstest/filesearch/AppFile.java
@@ -0,0 +1,13 @@
+package dstest.filesearch;
+
+/**
+ * Created by Administrator on 2016/8/5.
+ */
+public class AppFile{
+    String processName;
+    String filepath;
+    public AppFile(String processName,String filepath){
+        this.processName = processName;
+        this.filepath = filepath;
+    }
+}
